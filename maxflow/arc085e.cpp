8a90de9d2a5abbe3730f6ab2881a0df35928cdc8
bokuranosenjou <otanirintarou@ooyarintarounoMacBook-Air.local>
2022-11-29 14:11:59 +0900

    umeru_moyasu

---
+++ maxflow/arc085e.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+istream &operator>>(istream &is, modint &a) { long long v; is >> v; a = v; return is; }
+ostream &operator<<(ostream &os, const modint &a) { return os << a.val(); }
+istream &operator>>(istream &is, modint998244353 &a) { long long v; is >> v; a = v; return is; }
+ostream &operator<<(ostream &os, const modint998244353 &a) { return os << a.val(); }
+istream &operator>>(istream &is, modint1000000007 &a) { long long v; is >> v; a = v; return is; }
+ostream &operator<<(ostream &os, const modint1000000007 &a) { return os << a.val(); } 
+
+typedef long long ll;
+typedef vector<vector<int>> Graph;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define rep(i,n) for (int i = 0;i < (int)(n); i++)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define my_sort(x) sort(x.begin(), x.end())
+#define my_max(x) *max_element(all(x))
+#define my_min(x) *min_element(all(x))
+#define cout_vector(x) rep(i,x.size()) cout << x[i] << ' ';
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int INF = (1<<30) - 1;
+const ll LINF = (1LL<<62) - 1;
+const int MOD = 998244353;
+const int MOD2 = 1e9+7;
+const double PI = acos(-1);
+vector<int> di = {1,0,-1,0};
+vector<int> dj = {0,1,0,-1};
+
+int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    int N; cin >> N;
+    vector<ll> a(N);
+    rep(i,N) cin >> a[i];
+    mf_graph<ll> g(N+2);
+    int s = 0;
+    int t = N+1;
+    ll BIG = 1e9;
+    for(int x=1;x<=N;x++){
+        g.add_edge(s,x,BIG+a[x-1]);
+        g.add_edge(x,t,BIG);
+        for(int i=2;i<=N;i++){
+            if (i * x > N)continue;
+            g.add_edge(i*x,x,LINF);
+        }
+    }
+    ll wa = BIG * N + accumulate(all(a),0ll);
+    cout << wa - g.flow(s,t) << endl;
+}
